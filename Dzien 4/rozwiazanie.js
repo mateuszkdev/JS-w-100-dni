25a8bce57b5bd258aa315ca30c96092c7e046777
Mateusz Kalaga <46076250+mateuszkdev@users.noreply.github.com>
2020-04-09 14:36:05 +0200

    hello dzien 4 - zadanie 1

---
+++ Dzien 4/rozwiazanie.js
@@ -0,0 +1,18 @@
+
+//deklaracja stałej PLN
+const PLN = parseFloat(prompt("Podaj kwotę w PLN:")) 
+
+// deklaracja obiektu 'waluty' a w nim wartość euro i usd w stosunku do PLN
+const waluty = {
+    EURO: 5.20,
+    USD: 4.30
+}
+
+// deklaracja obiekty 'wynik', w nim przeliczone waluty na podstawie kwoty w PLN
+const wynik = {
+    EURO: PLN/waluty.EURO,
+    USD: PLN/waluty.USD
+}
+
+// wypisanie na ekran wyniku ( zapis z ES6 )
+document.write(`Euro: ${wynik.EURO} <br> Dolar: ${wynik.USD}`)
